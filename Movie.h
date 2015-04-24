6de193210eb340ff815eef36211ab4582bcba183
Cody Charland <Cody.Charland@colorado.edu>
2015-04-23 21:23:09 -0600

    Added function prototypes

--- Movie.h
+++ Movie.h
@@ -1,5 +1,7 @@
 #ifndef MOVIE_H
 #define MOVIE_H
+#include <string>
+#include <vector>
 
 struct Moovie
 {
@@ -19,8 +21,23 @@ class Movie
 {
     public:
         Movie();
+        void addMovie(std::string title, std::string genre, bool seen, int rating);
+        void addBook(std::string title, std::string genre, bool read, int rating);
+        void deleteMovie(std::string title);
+        void deleteBook(std::string title);
+        void writeToFile();
+        void readFromFile();
+        Moovie findMovie(std::string title);
+        Book findBook(std::string title);
+        Book randBook(std::string genre);
+        Moovie randMovie(std::string genre);
+        void updateBook(std::string title);
+        void updateMovie(std::string title);
+
     protected:
     private:
+        std::vector<Book> Books;
+        std::vector<Moovie> Movies;
 };
 
 #endif // MOVIE_H
